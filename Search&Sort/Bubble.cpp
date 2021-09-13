f3cd4936a00d69a183f335914041573eac8fc889
Md. Sakib Al Hasan <sakibalhasan17442@gmail.com>
2021-09-13 12:48:44 +0600

    Added Search and Sort

---
+++ Search&Sort/Bubble.cpp
@@ -0,0 +1,25 @@
+// Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int *xp, int *yp) {
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+void bubbleSort(int arr[], int n) {
+	int i , j;
+	for(i = 0; i < n - 1; i++) 
+		for(j = 0; i < n - i - 1; j++) if(arr[j] > arr[j+1]) swap(&arr[j], &arr[j+1]);
+}
+
+void printArray(int arr[], int size ) {
+	int i;
+	for(i = 0; i < size; i++) cout << arr[i] << " ";
+	cout << endl;
+}
+
+int main() {
+
+	return 0;
+}
---
+++ rad.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define mod 1e9 + 7
+#define fix(X) fixed << setprecision(X)
+#define inf 1e18
+
+void c_p_c() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+void solve(){
+
+}
+
+int main(){
+	c_p_c();
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
+	return 0;
+}
