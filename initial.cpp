d313e94e2ce98f679b0a3e3388ec11db663dcd88
Md. Sakib Al Hasan <sakibalhasan17442@gmail.com>
2021-10-24 15:58:50 +0600

    Update initial.cpp

--- initial.cpp
+++ initial.cpp
@@ -1,34 +1,19 @@
-// Bismillahir Rahmanir Rahim
-// Code by Sakib Al Hasan
 #include<bits/stdc++.h>
 using namespace std;
 
 #define ll long long
-#define ff(i, n) for(int i = 0; i < n; i++)
-#define ars(i, n, arr) for(int i = 0; i< n; i++) cin >> arr[i];
-#define an &&
-#define ro ||
-#define vi vector<int>
-#define pb(x) push_back(x)
-#define pf(x) push_front(x)
+#define ff(i, n ) for(int i = 0; i < n; i++) 
 
 const ll inf = 1e18;
 const int mod = 1e9+7;
 
-void finout() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-}
-
 void solve() {
-
+	
 }
 
 int main() {
-	finout();
 	int cases;
 	cin >> cases;
-	while(cases--) solve();
+	while(cases--) solve();	
 	return 0;
 }
